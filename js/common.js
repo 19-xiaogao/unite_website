f15ea45c1bf5d93548c62f70491e62d9632c7e7b
xiaolong <longjiuwei999@163.com>
2023-05-15 15:37:01 +0800

    优化样式

---
+++ js/common.js
@@ -0,0 +1,28 @@
+
+$('.header-logo').on('click', function () {
+    window.location.href = window.origin + '/index.html';
+})
+
+$('#news-center').on('click', function () {
+    window.location.href = window.origin + '/news_center.html';
+})
+
+$('#important-activity').on('click', function () {
+    window.location.href = window.origin + '/news_center_activity.html';
+})
+
+$('#important-news').on('click', function () {
+    window.location.href = window.origin + '/Important_news_center.html';
+})
+$('#cooperative').on('click', function () {
+    window.location.href = window.origin + '/cooperative_institution.html';
+})
+$('#attention').on('click', function () {
+    window.location.href = window.origin + '/attention.html';
+})
+$('#member').on('click', function () {
+    window.location.href = window.origin + '/member_news_center.html';
+})
+$('#about-me').on('click', function () {
+    window.location.href = window.origin + '/about_me.html';
+})
--- js/watchScroll.js
+++ js/watchScroll.js
@@ -12,9 +12,12 @@ function isScrollY() {
     console.log(pageHeight);
     if (pageHeight <= 850) {
         footer.css('position', `static`);
+        footer.css('margin-top', `28px`);
+
     } else {
         footer.css('position', `fixed`);
         footer.css('bottom', `0`);
+        footer.css('margin-top', `28px`);
     }
 }
 isScrollY()
