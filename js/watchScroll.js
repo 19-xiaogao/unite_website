787531dae03efe844316a2d0820a5fb2bfd812c6
xiaolong <longjiuwei999@163.com>
2023-05-15 15:05:53 +0800

    编写关注页面

--- js/watchScroll.js
+++ js/watchScroll.js
@@ -3,14 +3,18 @@ const footer = $('.footer');
 window.addEventListener('scroll', function () {
     isScrollY()
 });
+window.addEventListener('resize', function () {
+    isScrollY()
+})
 
 function isScrollY() {
-    const sy = window.scrollY;
-    if (sy <= 0) {
+    const pageHeight = document.documentElement.scrollHeight;
+    console.log(pageHeight);
+    if (pageHeight <= 850) {
+        footer.css('position', `static`);
+    } else {
         footer.css('position', `fixed`);
         footer.css('bottom', `0`);
-    } else {
-        footer.css('position', `static`);
     }
 }
 isScrollY()
